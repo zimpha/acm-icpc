0e1c3ea1ef615a20c893c5b6174248ada0ecb534
zimpha <zimpha@gmail.com>
2013-07-05 18:46:35 +0800

    modified:   USACO/2001/winter/green/bed1.cpp

--- USACO/2001/winter/green/bed1.cpp
+++ USACO/2001/winter/green/bed1.cpp
@@ -1,40 +1,36 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 
 const int MAXN=5005;
 const int SIZE=1000005;
 
-bool hash[SIZE];
+bool diff[SIZE];
 int S[MAXN];
-int N, K;
+int N, K, M;
 
 int main()
 {
 	freopen("bed1.in", "r", stdin);
 	freopen("bed1.out", "w", stdout);
 	scanf("%d", &N);
+	M=0;
 	for (int i=0; i<N; i++)
-		scanf("%d", &S[i]);
-	K=N;
-	memset(hash, 0, sizeof(hash));
-	while (1)
 	{
-		int count=0, top=0;
-		for (int i=0; i<N; i++)
-		{
-			if (hash[S[i]%K])
-			{
-				for (int j=0; j<i; j++)
-					hash[S[j]%K]=0;
-				break;
-			}
-			else
-			{
-				hash[S[i]%K]=1;
-				count++;
-			}
-		}
-		if (count==N) break;
+		scanf("%d", &S[i]);
+		if (S[i]>M) M=S[i];
+	}
+	memset(diff, 0, sizeof(diff));
+	for (int i=0; i<N; i++)
+		for (int j=i+1; j<N; j++)
+			diff[abs(S[i]-S[j])]=true;
+	K=N;
+	while (K<=M)
+	{
+		bool flag=true;
+		for (int i=K; i<=M&&flag; i+=K)
+			if (diff[i]) flag=false;
+		if (flag) break;
 		K++;
 	}
 	printf("%d\n", K);
